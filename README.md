7791ccc40463849a0407dbe763a9e9440861f3d8
alek3y <alek3y@protonmail.com>
2020-10-27 18:49:16 +0100

    ♻️ Rewrote everything from scratch
    Very much needed :I

--- README.md
+++ README.md
@@ -4,58 +4,71 @@ Parse the arguments from `sys.argv` or print the usage.
 ## Class parameters
 The only parameter of the class is `info`.
 
-One simple example would be from my [4chan-d](https://github.com/AleK3y/4chan-d) repository:
+One simple example would be this one:
 ```python
 {
-    "usage": "4chan-d.py [OPTIONS]",    # Executable syntax
-    "description": "Download threads from 4chan.",    # New lines with "\n" are automatically formatted
-  
-    # Options with parameters
-    "parameters": {
-        "-u": {
-            "required": True,    # Tell whether the option is obligatory or not
-            "description": "4chan thread url",
-        },
-        "-o": {
-            "required": False,
-            "description": "output parent directory",    # " (optional)" will be appended if it isn't required
-        },
-    },
+	#"usage": "",		# A default usage will be computed if omitted
+	"description": "Calculate the power.",
+	"arguments": {
 
-    # Single options
-    "options": {
-        "-f": {
-            "required": False,
-            "description": "if enabled the file names will be the uploaded ones",
-        },
-    }
+		# Positional arguments are always required
+		"positional": {
+			"BASE": {
+				"description": ""
+			},
+			"EXPONENT": {
+				"description": ""
+			}
+		},
+
+		"parameters": {
+			"-o": {
+				"aliases": ["--output"],
+				"name": "FILE",
+				"description": "set the file to write to",
+				"required": True,
+			}
+		},
+
+		# Options are never required
+		"options": {
+			"-v": {
+				"aliases": ["--verbose"],
+				"description": "report more info about the operations",
+			}
+		}
+	}
 }
 ```
 
-## Missing required parameters
-If a required parameter is missing, the `update_args` function will throw a `MissingMandatoryArgument` exception.
+## Exceptions
+After initializing the class you'll have to run the `update` function. The function might throw these exceptions:
+- `MissingRequiredArgument`: When a required or positional parameter is missing
+- `WrongArgumentsPlacement`
+- `TooFewArguments`: When the amount of positional parameters is lower than the expected
 
 ## Example
 Following the information dictionary structure you can initialize the class like this:
 ```python
-import args
+from args import Arguments
 
 # infos = ...
 
-parser = args.Arguments(infos)
+parser = Arguments(infos)
 try:
-    parser.update_args()
-except args.Arguments.MissingMandatoryArgument:
-    parser.usage()    # Print the usage if one or more are missing
-    exit()
+	parser.update()
+except (Arguments.MissingRequiredArgument, Arguments.WrongArgumentsPlacement, Arguments.TooFewArguments):
+	parser.usage()
+	exit()
 ```
 
 ## Access the arguments
 You can either access the dictionary from `parser.args` or `parser.get_args()`. \
-An example of how its structure would be:
+An example of how it would be:
 ```python
 {
-    "-u": "https://boards.4channel.org/g/thread/51971506",    # For parameters
-    "-f": None,    # For options, the value (None) should be ignored
+	"positional": ["2", "4"],
+	"parameters": [{"-o": "file"}],
+	"options": []
 }
 ```
--- __init__.py
+++ __init__.py
@@ -1,75 +1,172 @@
-#!/usr/bin/python3
-
-import sys
-
 class Arguments:
 
-	INDENTATION = 4
+	##
+	# Attributes
+	#
 
+	INDENTATION = 6
+	DEFAULT_NAME = "VALUE"
+	MAX_USAGE_PARAMETERS = 4
+
+	info = None
+	args = {
+		"positional": [],
+		"parameters": [],
+		"options": []
+	}
+
+	##
 	# Exceptions
-	class MissingMandatoryArgument(Exception):
-		def __init__(self, arg):
-			self.strerror = arg
-			self.args = {arg}
+	#
 
-	info = {}
-	args = {}
+	class MissingRequiredArgument(Exception):
+		pass
+	class WrongArgumentsPlacement(Exception):
+		pass
+	class TooFewArguments(Exception):
+		pass
+
+	##
+	# Functions
+	#
 
 	def __init__(self, info):
+		if "arguments" not in info:
+			raise ValueError()
+
 		self.info = info
+		info_args = self.info["arguments"]
+
+		# Add missing sections
+		for arg_type in self.args.keys():
+			if arg_type not in info_args:
+				info_args[arg_type] = {}
+
+		# Add missing names on parameters
+		for arg, value in info_args["parameters"].items():
+			if "name" not in value:
+				value["name"] = self.DEFAULT_NAME
 
 	def get_args(self):
 		return self.args
 
-	def update_args(self):
-		"""
-		Update the arguments from sys.argv.
-		"""
+	def get_info(self):
+		return self.info
 
-		i = 0
+	def update(self):
+		import sys
+
+		info_args = self.info["arguments"]
+
+		## Parse the arguments ##
+
+		i = 1
 		while i<len(sys.argv):
+			arg = sys.argv[i]
 
-			# Check for the options with parameters
-			if sys.argv[i] in self.info["parameters"]:
-				self.args[sys.argv[i]] = sys.argv[i+1]
-				i += 1		# Skip the parameter
+			# Parameters
+			if arg in info_args["parameters"]:
+				if i+1 >= len(sys.argv):
+					raise self.WrongArgumentsPlacement(arg)
 
-			# Check for single options
-			elif sys.argv[i] in self.info["options"]:
-				self.args[sys.argv[i]] = None		# Options which are supposed to go alone don't have parameters
+				self.args["parameters"].append({arg: sys.argv[i+1]})
+				i += 1
+
+			# Options
+			elif arg in info_args["options"]:
+				self.args["options"].append(arg)
+
+			# Positional parameters
+			else:
+				self.args["positional"].append(arg)
 
 			i += 1
 
-		# Check if there are mandatory arguments missing
-		for arg in self.info["parameters"]:
-			if arg not in self.args and self.info["parameters"][arg]["required"]:
-				raise self.MissingMandatoryArgument(f"'{arg}' is missing")
+		## Check for missing parameters ##
 
-		# obligatory args on options is kinda useless
-		for arg in self.info["options"]:
-			if arg not in self.args and self.info["options"][arg]["required"]:
-				raise self.MissingMandatoryArgument(f"'{arg}' is missing")
+		for arg, value in info_args["parameters"].items():
+			parsed = False
+			for parsed_arg in self.args["parameters"]:
+				if list(parsed_arg.keys())[0] == arg:
+					parsed = True
+					break
+
+			if "required" in value and value["required"] and not parsed:
+					raise self.MissingRequiredArgument(arg)
+
+		if len(self.args["positional"]) < len(info_args["positional"].keys()):
+			raise self.TooFewArguments()
 
 	def usage(self):
-		"""
-		Print the usage with descriptions.
-		"""
+		import sys
 
-		print("Usage: " + self.info["usage"])
-		
-		if self.info["description"]:
-			for line in self.info["description"].split("\n"):
-				print(" "*self.INDENTATION + line)
+		info_args = self.info["arguments"]
 
-		print()
+		## Usage ##
 
-		params = self.info["parameters"]
-		opts = self.info["options"]
-		merged = dict(list(params.items()) + list(opts.items()))
+		try:
+			usage = self.info["usage"]
+		except KeyError:
+			info_args = self.info["arguments"]
+			usage = sys.argv[0]
 
-		print(" "*self.INDENTATION + "Options:")
-		for arg in merged:
-			print(
-				" "*(self.INDENTATION+1) + arg +
-				" "*self.INDENTATION*2 + merged[arg]["description"]
-			)
+			# Add options to usage
+			if len(info_args["options"].keys()) > 0:
+				usage += " [OPTION]"
+
+			required_count = 0
+			for arg, value in info_args["parameters"].items():
+				if "required" in value and value["required"]:
+					required_count += 1
+
+			# Add required parameters to usage if they're not too many
+			if required_count <= self.MAX_USAGE_PARAMETERS:
+				for arg, value in info_args["parameters"].items():
+					if "required" in value and value["required"]:
+						usage += " " + arg + " " + value["name"].upper()
+
+			# Add positional parameters to usage line
+			for arg, value in info_args["positional"].items():
+				usage += " " + arg.upper()
+
+		print(f"Usage: {usage}")
+		print(
+			" "*self.INDENTATION +
+			((self.info["description"] + "\n") if "description" in self.info else "")
+		)
+
+		## Arguments help ##
+
+		for arg_type in self.args.keys():
+			if len(info_args[arg_type].keys()) > 0:
+				print(" "*self.INDENTATION + arg_type.title() + ":")
+
+				# Create a list of aliases and descriptions to print
+				help_args = []
+				for arg, value in info_args[arg_type].items():
+					help_args.append([
+						(
+							(", ".join([arg] + value["aliases"]) if "aliases" in value else arg) + " " +		# Arguments and aliases
+							(value["name"] if "name" in value else "").upper()		# Name
+						),
+						value["description"] if "description" in value else ""
+					])
+
+				# Find the longest argument line
+				longest_arg = 0
+				for arg in help_args:
+					if len(arg[0]) > longest_arg:
+						longest_arg = len(arg[0])
+
+				# Pad the argument and print them
+				for arg in help_args:
+					padding = self.INDENTATION*4 - len(arg[0])
+					if padding <= 1:
+						padding = 2
+
+					print(
+						" "*(self.INDENTATION+1) + arg[0] +		# Argument and aliases
+						" "*padding + arg[1]		# Description
+					)
+
+				print()
